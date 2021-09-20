d01c2eea738b9b8794860b35fc6eee8b9612dfcd
Benito Sam <beniberyl54@gmail.com>
2021-09-20 11:39:52 +0530

    Finished log-in/log-out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 
-  #Return full titlr
+  # Return full title
   def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial"
     if page_title.empty?
       base_title
     else
-      page_title + "|" + base_title
+      page_title + " | " + base_title
     end
   end
 end
